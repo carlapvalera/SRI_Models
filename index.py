8ace84828484d3024f7c73ef2170c3b7fcc77f1e
Francisco Vicente Suarez Bellon <95106875+FVSB@users.noreply.github.com>
2024-03-06 02:28:18 -0500

    Add Stadistic Model  Okapi

---
+++ index.py
@@ -0,0 +1,107 @@
+from ir_measures import *
+from cranfield import CranfieldIR
+from vs_model import ClassVectorSpaceModel
+from pln import SimpleTextProcessor
+import streamlit as st
+from Utils import Query
+
+
+
+def init_session():
+    st.session_state['text_query'] = ''
+    st.session_state['model'] = None
+    st.session_state['corpus'] = None
+    st.session_state['system'] = None
+    st.session_state['result'] = []
+    st.session_state['relevants'] = {}
+
+
+def clean_checkboxes():
+    for i in range(len(st.session_state['result'])):
+        st.session_state[f"rel{i}"] = False
+        st.session_state[f'doc{i}'] = False
+
+
+def get_irsystem(model, corpus):
+    if st.session_state['model'] == model and st.session_state['corpus'] == corpus:
+        return st.session_state['system']
+
+    st.session_state['model'] = model
+    st.session_state['corpus'] = corpus
+
+    text_processor = SimpleTextProcessor()
+    if model == 'ClassVectorSpace':
+        model = ClassVectorSpaceModel()
+
+
+    if corpus == "Cranfield":
+        st.session_state['system'] = CranfieldIR(model, text_processor)
+
+
+
+if 'system' not in st.session_state:
+    init_session()
+
+st.title('Welcome to the Information Retrieval System! 👋')
+
+st.sidebar.header('System options')
+corpus = st.sidebar.radio("Select corpus", ("Cranfield"))
+model = st.sidebar.radio("Select model", ("ClassVectorSpace"))
+action = st.sidebar.radio("Select mode", ("Retrieve query"))
+
+if action == "Retrieve query":
+    query = st.text_input('What are you searching for?')
+    if query != st.session_state['text_query']:
+        clean_checkboxes()
+        st.session_state['text_query'] = query
+        query_split = st.session_state['text_query'].split()
+        query = Query(1, query_split)
+
+        # clean_checkboxes()
+        irsystem = get_irsystem(model, corpus)
+        scores_docs = irsystem.model.retrieve_query(query)
+
+        st.session_state['result'] = []
+        for item in scores_docs:
+            st.session_state['result'].append(irsystem.get_doc(item.doc_id))
+
+    if st.sidebar.button("Improve"):
+        clean_checkboxes()
+        if len(st.session_state['result']) != 0:
+            relevants = []
+            for i in range(len(st.session_state['result'])):
+                if st.session_state['relevants'][i]:
+                    relevants.append(st.session_state['result'][i].doc_id)
+
+            if len(relevants) != 0:
+                irsystem = get_irsystem(model, corpus)
+                scores_docs = irsystem.model.retrieve_feedback(query, relevants, st.session_state['result'])
+
+                st.session_state['result'] = []
+                for item in scores_docs:
+                    st.session_state['result'].append(irsystem.get_doc(item.doc_id))
+
+    if st.session_state['text_query']:
+        layout = [1, 1.5, 21]
+        num_col, col1, colt = st.columns(layout)
+        col1.text('Open')
+        colt.text('Title of document')
+        for i, r in enumerate(st.session_state['result']):
+            col3, col4, col6 = st.columns(layout)
+            col3.text(f'{i}. ')
+            globals()[f"doc{i}"] = col4.checkbox(label='', key=f"doc{i}")
+
+            try:
+                col6.text(r.title)
+            except:
+                try:
+                    col6.text(r.text)
+                except:
+                    col6.text(r.text)
+            if globals()[f"doc{i}"]:
+                _, _, doc_text = st.columns(layout)
+                doc_text.markdown(r.text)
+                _, rel_message, rel_answer, _ = st.columns([3.5, 9.5, 1, 8.5])
+                rel_message.text('Was this document helpful to you?')
+                st.session_state['relevants'][i] = rel_answer.checkbox(label='', key=f"rel{i}", value=False)
+
---
+++ stadistic_model.py
@@ -0,0 +1,61 @@
+from typing import Counter
+from vectorial_model import FrequencyModel
+from ir_measures import ScoredDoc
+import numpy as np
+from utils import Query
+import math
+
+
+class OkapiBM25Model(FrequencyModel):
+    def __init__(self):
+        super().__init__()
+        self.k1 = 1.2
+        self.b = 0.75
+        self.k3 = 7
+
+    def pseudo_feedback(self):
+        relevants_by_term = {}
+        nrelevants_by_term = {}
+        VR = set()
+        for term in self.idf:
+            VRt, VNRt = self.feedback_on_term(term, sets_diff=False, only_lens=False)
+            VR.update(VRt)
+            relevants_by_term[term] = len(VRt)
+            nrelevants_by_term[term] = len(VNRt)
+
+        VR = len(VR)
+        for term in self.idf:
+            VRt, VNRt = relevants_by_term[term], nrelevants_by_term[term]
+            if not VRt or not VNRt:
+                return self.idf[term]
+            nom = (VRt + 0.5) / (VNRt + 0.5)
+            den = (self.df[term] - VRt + 0.5) / (self.corpus_size - self.df[term] - VR + VRt + 0.5)
+            self.idf[term] = math.log(nom / den)
+
+    def dscore(self, term: int, doc: int) -> float:
+        nom = self.idf[term] * (self.k1 + 1) * self.tf[term][doc]
+        den = self.k1 * ((1-self.b) + self.b*(self.doc_len[doc]/self.avg_doc_len)) + self.tf[term][doc]
+        return nom / den
+
+    def qscore(self, query):
+        query_tf = Counter(query)
+        w_tq = {}
+        for term, freq in query_tf.items():
+            nom = (self.k3 + 1) * freq
+            den = self.k3 + freq
+            w_tq[term] = nom / den
+        return lambda t : w_tq[t]
+
+    def tf_function(self, term: int, doc: int):
+        return self._tf_n(term, doc)
+
+    def idf_function(self, term: int):
+        return self._idf_p(term, 0.5)
+
+    def retrieve_query(self, query: Query):
+        query_id = query.id
+        query = [self.vocabulary[word] for word in query if word in self.vocabulary]
+        return [
+            ScoredDoc(query_id, str(doc), score)
+            for score, doc in self.daat_kretrieve(query, 15)
+        ]
